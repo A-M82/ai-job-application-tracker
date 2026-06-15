36782c7239c9db35adff4820c1af589589e3d4ee
Ahmed Madyan <your-email@example.com>
2026-06-15 13:26:27 +0200

    Add application table to dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,46 @@
 function Dashboard() {
+  const applications = [
+    {
+      company: 'Volkswagen',
+      position: 'Project Manager',
+      status: 'Interview',
+    },
+    {
+      company: 'Siemens Energy',
+      position: 'Senior Project Manager',
+      status: 'Applied',
+    },
+    {
+      company: 'Mapbox',
+      position: 'Principal Technical Program Manager',
+      status: 'Rejected',
+    },
+  ]
+
   return (
     <section>
       <h2>Application Dashboard</h2>
       <p>Overview of current job applications and their status.</p>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Company</th>
+            <th>Position</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {applications.map((application, index) => (
+            <tr key={index}>
+              <td>{application.company}</td>
+              <td>{application.position}</td>
+              <td>{application.status}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </section>
   )
 }
